e9d2adeeba1bf9252bc9f64e6d05803cd1439d79
kenshiro.sugiyama <kenshi.0223@icloud.com>
2019-10-04 12:23:17 +0000

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/new'
+  get 'users/create'
   root to: 'tasks#index'
 
-  
+  get 'signup', to: 'users#new'
   resources :tasks
+  resources :users, only: [:new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
